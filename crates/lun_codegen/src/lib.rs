318309af3f70253050d67d02f02dc1419895b86e
Thibault <th1bault.vincent@proton.me>
2025-06-11 20:34:42 +0200

    removed the code generator code that will no longer word with the new instructions

--- crates/lun_codegen/src/lib.rs
+++ crates/lun_codegen/src/lib.rs
@@ -24,19 +24,13 @@ impl CodeGenerator {
     }
 
     pub fn produce(&mut self) -> StageResult<LunBin> {
+        todo!("REMAKE THE CODEGEN");
+
+        #[allow(unreachable_code)]
+        let _ = &self.ast; // REMOVE ME AFTER
+
         let mut builder = LunBinBuilder::new();
         builder.typ(self.typ);
-        // TODO: make a simpler IR
-
-        let CkStmt::Assignement {
-            variable: _,
-            ref value,
-        } = self.ast.stmts[0].stmt
-        else {
-            todo!();
-        };
-
-        self.gen_expr(value.clone()).unwrap();
 
         builder.section(LunBin::SECTION_CODE, self.bc.code_data());
 
@@ -44,47 +38,5 @@ impl CodeGenerator {
             return StageResult::Fail(self.sink.clone());
         }
         StageResult::Good(builder.build().unwrap())
-    }
-
-    pub fn gen_expr(&mut self, expr: CkExpression) -> Result<(), Diagnostic> {
-        match expr.expr {
-            CkExpr::IntLit(_) => {
-                todo!()
-            }
-            CkExpr::BoolLit(_) => {
-                // TODO: make only two, constants true and false, no need to
-                // create new ones each time, or create a new opcode
-                todo!("bool literal compilation")
-            }
-            CkExpr::StringLit(_) => {
-                todo!("string literal compilation")
-            }
-            CkExpr::Grouping(expr) => {
-                self.gen_expr(*expr)?;
-            }
-            CkExpr::Ident(_) => todo!("ident expr"),
-            CkExpr::Binary { lhs, op, rhs } => {
-                self.gen_expr(*lhs)?;
-                self.gen_expr(*rhs)?;
-
-                match op {
-                    BinOp::Add => self.bc.write_add(),
-                    BinOp::Sub => self.bc.write_sub(),
-                    BinOp::Mul => self.bc.write_mul(),
-                    BinOp::Div => self.bc.write_div(),
-                    _ => todo!("implement comparaison operators"),
-                }
-            }
-            CkExpr::Unary { op, expr } => {
-                self.gen_expr(*expr)?;
-
-                match op {
-                    UnaryOp::Negation => self.bc.write_neg(),
-                    UnaryOp::Not => todo!("not operator compilation"),
-                }
-            }
-            _ => todo!("other expression to compile"),
-        }
-        Ok(())
     }
 }
