132ae0fd9318b104dcc06e45e4f926607e50bac3
Thibault <th1bault.vincent@proton.me>
2025-06-16 18:32:16 +0200

    correctly implement Block

--- crates/lun_parser/src/stmt.rs
+++ crates/lun_parser/src/stmt.rs
@@ -1,23 +1,92 @@
 //! Parsing of lun's statements and chunk.
 
+use std::hint::unreachable_unchecked;
+
 use super::*;
 
 /// Block of Lun statements
 #[derive(Debug, Clone)]
 pub struct Block {
     pub stmts: Vec<Statement>,
+    pub last_expr: Option<Box<Expression>>,
     pub loc: Span,
 }
 
 impl AstNode for Block {
     fn parse(parser: &mut Parser) -> Result<Self, Diagnostic> {
+        let mut stmts = Vec::new();
+
         let (_, lo) =
             expect_token!(parser => [Punct(Punctuation::LBrace), ()], Punctuation::LBrace);
+
+        let mut last_expr = None;
+
+        loop {
+            match parser.peek_tt() {
+                Some(EOF) | Some(Punct(Punctuation::RBrace)) | None => {
+                    break;
+                }
+                _ => {}
+            }
+
+            let stmt = parse!(parser => Statement);
+
+            match parser.peek_tt() {
+                Some(Punct(Punctuation::RBrace)) => break,
+                Some(Punct(Punctuation::SemiColon)) => {
+                    let next_brace = if let Some(Punct(Punctuation::RBrace)) = parser.peek_tt() {
+                        true
+                    } else {
+                        false
+                    };
+                    let is_expr = stmt.is_expr();
+
+                    match (next_brace, is_expr) {
+                        (true, true) => {
+                            // here we found the last expression, because the
+                            // following token is } and the last "statement" was
+                            // an expression.
+                            let Statement {
+                                stmt: Stmt::Expression(expr),
+                                loc: _,
+                            } = stmt
+                            else {
+                                // NOTE: here we are fine because, we checked that stmt is an expr before.
+                                unsafe { unreachable_unchecked() }
+                            };
+
+                            last_expr = Some(Box::new(expr));
+                            break;
+                        }
+                        // here, the next thing is a brace so, no need to keep parsing there is nothing more.
+                        (true, false) => break,
+                        (false, _) => {
+                            expect_token!(parser => [Punct(Punctuation::SemiColon), ()], Punct(Punctuation::SemiColon));
+                        }
+                    }
+                }
+                Some(_) => {
+                    let t = parser.peek_tok().unwrap();
+
+                    return Err(ExpectedToken::new(
+                        [Punct(Punctuation::SemiColon), Punct(Punctuation::RBrace)],
+                        t.tt.clone(),
+                        Some("block"),
+                        t.loc.clone(),
+                    )
+                    .into_diag());
+                }
+                None => return Err(parser.eof_diag()),
+            }
+            stmts.push(stmt);
+        }
+
         let (_, hi) =
             expect_token!(parser => [Punct(Punctuation::RBrace), ()], Punctuation::RBrace);
 
         Ok(Block {
-            stmts: Vec::new(),
+            stmts,
+            last_expr,
             loc: Span::from_ends(lo, hi),
         })
     }
@@ -28,6 +97,12 @@ impl AstNode for Block {
 pub struct Statement {
     pub stmt: Stmt,
     pub loc: Span,
+}
+
+impl Statement {
+    pub fn is_expr(&self) -> bool {
+        matches!(self.stmt, Stmt::Expression(_))
+    }
 }
 
 #[derive(Debug, Clone)]
