e836890d65f3c404339abcbf0afba0126c07e96a
Thibault <th1bault.vincent@proton.me>
2025-09-27 15:18:47 +0200

    chore: remove the old FIR stage

--- compiler/lunc_codegen/src/lib.rs
+++
@@ -1,4 +0,0 @@
-//! Code generation of lun, takes [FIR](lunc_fir) and compiles it to machine code.
-#![doc(
-    html_logo_url = "https://raw.githubusercontent.com/lunprog/lun/main/src/assets/logo_no_bg_black.png"
-)]
